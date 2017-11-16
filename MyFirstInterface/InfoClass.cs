e27cee4f4fc9909f388f7427fd60a42d4e0a11c1
billy <belawel@hotmail.com>
2017-11-16 14:45:27 +0100

    Aladin was here

---
+++ MyFirstInterface/InfoClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstInterface
+{
+    class InfoClass : IFirstinterface
+    {
+        private string name;
+        private int shoes;
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public int Shoes
+        {
+            set { shoes = value; }
+            get { return shoes;  }
+        }
+        public string GetInfo()
+        {
+            return string.Format("Hej {0} nå så du bruger nr {1} i sko.", name , shoes);
+        }
+        Console.ReadLine();
+    }
+}
---
+++ MyFirstInterface/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InfoClass info1 = new InfoClass();
+            info1.Name = "Jesper";
+            info1.Shoes = 47;
+
+            IFirstinterface if1 = info1;
+
+            Console.WriteLine(info1.GetInfo());
+            Console.WriteLine(if1.GetInfo());
+
+            //////////////////////////////
+            InfoClass info2 = new InfoClass();
+            info2.Name = "";
+            info2.Shoes = 47;
+
+            Object o1 = info2;              
+
+            if (o1 is IFirstinterface)
+            {
+                IFirstinterface if2 =  (IFirstinterface)o1;
+                Console.WriteLine(if2.GetInfo());
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
